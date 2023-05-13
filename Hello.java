caf05ef05e53c36ae0194ea6ef2cdd00e5f71b67
obiomap <obiomap@gmail.com>
2023-05-12 23:18:51 -0400

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class hello {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		int x = 10;
+		int y = 20
+		
+		int sum = x+y;
+		
+		System.out.println("The sum of x and y is "+sum);
+	}
+}
